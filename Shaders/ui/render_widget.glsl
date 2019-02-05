f1b1d9ac53b46544b2fbdff2172bcf8aa16504fe
Ubuntunux <ubuntunux@gmail.com>
2019-02-05 15:57:41 +0900

    resources

---
+++ Shaders/ui/render_widget.glsl
@@ -0,0 +1,30 @@
+#include "ui/ui_quad.glsl"
+
+uniform sampler2D texture_diffuse;
+uniform bool is_render_diffuse;
+uniform vec4 color;
+uniform vec4 texcoord;
+
+
+#ifdef FRAGMENT_SHADER
+layout (location = 0) in VERTEX_OUTPUT vs_output;
+layout (location = 0) out vec4 fs_output;
+
+void main()
+{
+    vec4 result = color;
+
+    vec2 uv = mix(texcoord.xy, texcoord.zw, vs_output.tex_coord.xy);
+
+    if(is_render_diffuse)
+    {
+        if(0.0 < uv.x && 0.0 < uv.y && uv.x < 1.0 && uv.y < 1.0)
+        {
+            result = texture2D(texture_diffuse, uv);
+            result = mix(result, result * color, color.w);
+        }
+    }
+
+    fs_output = result;
+}
+#endif

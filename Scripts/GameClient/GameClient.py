525fc81345d94db7a72a3917d8cd741b84128f88
ubuntunux <ubuntunux@gmail.com>
2019-05-14 04:23:32 +0900

    update

--- Scripts/GameClient/GameClient.py
+++ Scripts/GameClient/GameClient.py
@@ -2,7 +2,13 @@ import numpy as np
 
 from PyEngine3D.App.GameBackend import Keyboard
 from PyEngine3D.Common import logger
-from PyEngine3D.Utilities import Singleton, StateMachine, StateItem
+from PyEngine3D.Utilities import Singleton, StateMachine, StateItem, Float3
+
+
+GRAVITY = 20.0
+JUMP_SPEED = 10.0
+MOVE_SPEED = 10.0
+BOUND_BOX_OFFSET = [0.0, 0.1, 0.0]
 
 
 class GameClient(Singleton):
@@ -12,11 +18,10 @@ class GameClient(Singleton):
         self.resource_manager = None
         self.scene_manager = None
         self.player = None
-        self.jump = False
         self.punch = False
         self.kick = False
-        self.vel = 0.0
-
+        self.on_ground = False
+        self.velocity = Float3(0.0, 0.0, 0.0)
         self.animation_meshes = {}
 
     def initialize(self, core_manager):
@@ -53,6 +58,7 @@ class GameClient(Singleton):
         player_model = self.resource_manager.get_model("player")
         self.player = self.scene_manager.add_object(model=player_model, pos=pos)
         self.player.transform.set_scale(0.5)
+        self.velocity[...] = Float3(0.0, 0.0, 0.0)
 
         # fix camera rotation
         main_camera.transform.set_rotation((0.0, 1.57079, 0.0))
@@ -67,30 +73,39 @@ class GameClient(Singleton):
         btn_left, btn_middle, btn_right = self.game_backend.get_mouse_pressed()
         camera = self.scene_manager.main_camera
 
-        move_speed = 10.0 * delta
-        rotation_speed = 0.3141592 * delta
         move = False
 
-        if keydown[Keyboard.W] or self.game_backend.wheel_up:
-            self.player.transform.set_yaw(-1.57079)
-            move = True
-        elif keydown[Keyboard.S] or self.game_backend.wheel_down:
-            self.player.transform.set_yaw(1.57079)
-            move = True
+        KEY_FLAG_NONE = 0
+        KEY_FLAG_W = 1 << 0
+        KEY_FLAG_S = 1 << 1
+        KEY_FLAG_A = 1 << 2
+        KEY_FLAG_D = 1 << 3
+
+        key_map = dict()
+        key_map[KEY_FLAG_W] = -1.57079
+        key_map[KEY_FLAG_S] = 1.57079
+        key_map[KEY_FLAG_A] = 0.0
+        key_map[KEY_FLAG_D] = 3.141592
+        key_map[KEY_FLAG_W | KEY_FLAG_A] = -0.785395
+        key_map[KEY_FLAG_W | KEY_FLAG_D] = 3.926987
+        key_map[KEY_FLAG_S | KEY_FLAG_A] = 0.785395
+        key_map[KEY_FLAG_S | KEY_FLAG_D] = 2.356191
+
+        press_keys = 0
+
+        if keydown[Keyboard.W]:
+            press_keys |= KEY_FLAG_W
+        elif keydown[Keyboard.S]:
+            press_keys |= KEY_FLAG_S
 
         if keydown[Keyboard.A]:
-            self.player.transform.set_yaw(0.0)
-            move = True
+            press_keys |= KEY_FLAG_A
         elif keydown[Keyboard.D]:
-            self.player.transform.set_yaw(3.141592)
+            press_keys |= KEY_FLAG_D
+
+        if press_keys in key_map:
+            self.player.transform.set_yaw(key_map[press_keys])
             move = True
-
-        if move:
-            self.player.transform.move_front(move_speed)
-
-        if not self.jump and keydown[Keyboard.SPACE]:
-            self.jump = True
-            self.vel = 0.5
 
         if btn_left:
             self.punch = True
@@ -98,43 +113,60 @@ class GameClient(Singleton):
         if btn_right:
             self.kick = True
 
-        # Jump
-        player_pos = self.player.transform.get_pos()
+        if self.on_ground:
+            if keydown[Keyboard.SPACE]:
+                self.on_ground = False
+                self.velocity[1] = JUMP_SPEED
 
-        def check_collide(point, box):
-            for i in range(3):
-                if point[i] < box.bound_min[i] or box.bound_max[i] < point[i]:
-                    return False
-            return True
+            if move:
+                self.velocity[0] = self.player.transform.front[0] * MOVE_SPEED
+                self.velocity[2] = self.player.transform.front[2] * MOVE_SPEED
+            else:
+                self.velocity[0] = 0.0
+                self.velocity[2] = 0.0
 
-        collide = False
-        bound_box = self.player.bound_box
+        self.velocity[1] -= GRAVITY * delta
+
+        old_player_pos = self.player.transform.get_pos()
+        move_vector = self.velocity * delta
+        player_pos = old_player_pos + move_vector
+        self.on_ground = False
+
+        def compute_collide(i, old_position, position, move_vector, player_bound_box, bound_box):
+            j = (i + 1) % 3
+            k = (i + 2) % 3
+
+            def is_in_plane(index, ratio):
+                return bound_box.bound_min[index] < (old_position[index] + move_vector[index] * ratio + BOUND_BOX_OFFSET[index]) < bound_box.bound_max[index]
+
+            if move_vector[i] < 0.0 and position[i] <= bound_box.bound_max[i] <= old_position[i]:
+                ratio = abs((bound_box.bound_max[i] - old_position[i]) / move_vector[i])
+                if is_in_plane(j, ratio) and is_in_plane(k, ratio):
+                    move_vector[i] *= ratio
+                    position[i] = old_position[i] + move_vector[i]
+                    if 1 == i:
+                        self.on_ground = True
+            elif 0.0 < move_vector[i] and old_position[i] <= bound_box.bound_min[i] <= position[i]:
+                ratio = abs((bound_box.bound_min[i] - old_position[i]) / move_vector[i])
+                if is_in_plane(j, ratio) and is_in_plane(k, ratio):
+                    move_vector[i] *= ratio
+                    position[i] = old_position[i] + move_vector[i]
+
         for collision_actor in self.scene_manager.collision_actors:
             for geometry_bound_box in collision_actor.get_geometry_bound_boxes():
-                if check_collide([bound_box.bound_center[0], bound_box.bound_min[1], bound_box.bound_center[2]], geometry_bound_box):
-                    collide = True
-                    break
-            if collide:
-                break
+                for i in range(3):
+                    compute_collide(i, old_player_pos, player_pos, move_vector, self.player.model.mesh.bound_box, geometry_bound_box)
 
-        if self.jump or not collide:
-            self.vel -= 1.0 * delta
-            self.player.transform.move_y(self.vel)
-            self.player.set_animation(self.animation_meshes['jump'], loop=False, speed=1.0)
-        elif move:
-            self.player.set_animation(self.animation_meshes['walk'], loop=True)
+        if self.on_ground:
+            self.velocity[1] = 0.0
+            if move:
+                self.player.set_animation(self.animation_meshes['walk'], loop=True)
+            else:
+                self.player.set_animation(self.animation_meshes['idle'], loop=True, speed=0.3)
         else:
-            self.player.set_animation(self.animation_meshes['idle'], loop=True, speed=0.3)
+            self.player.set_animation(self.animation_meshes['jump'], loop=False, speed=1.0)
 
-        # if keydown[Keyboard.Q]:
-        #     self.player.transform.move_y(1.0)
-        # elif keydown[Keyboard.E]:
-        #     self.player.transform.move_y(-1.0)
-
-        if collide:
-            self.vel = 0.0
-            self.jump = False
-
+        self.player.transform.set_pos(player_pos)
         camera.transform.set_pos(player_pos)
         camera.transform.move_up(5.0)
         camera.transform.move_front(10.0)
